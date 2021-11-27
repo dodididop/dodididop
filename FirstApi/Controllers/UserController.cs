679373a5b62d9ae6972ecee34ef1fe36a7de4034
dodididop <digdemcokyasar@gmail.com>
2021-11-27 22:33:59 +0300

    created put, post and delete methods

--- FirstApi/Controllers/UserController.cs
+++ FirstApi/Controllers/UserController.cs
@@ -26,6 +26,44 @@ namespace FirstApi.AddControllers{
             var userList = UserList.OrderBy(x=>x.Id).ToList<User>();
             return userList;
         }
+
+        [HttpPost]
+
+        public IActionResult AddUser([FromBody] User newUser){
+            var user = UserList.SingleOrDefault(x=>x.Id == newUser.Id);
+
+            if(user is not null)
+                return BadRequest();
+            UserList.Add(newUser);
+            return Ok();
+
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult UpdateUser(int id,[FromBody] User updatedUser){
+            var user = UserList.SingleOrDefault(x=>x.Id == id);
+
+            if(user is null)
+                return BadRequest();
+            user.Name = updatedUser.Name != default ? updatedUser.Name : user.Name;
+            user.SurName = updatedUser.SurName != default ? updatedUser.SurName : user.SurName;
+            user.Password = updatedUser.Password != default ? updatedUser.Password : user.Password;
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult DeleteUser(int id){
+            var user = UserList.SingleOrDefault(x=>x.Id==id);
+
+            if(user is null)
+                return BadRequest();
+            
+            UserList.Remove(user);
+            return Ok();
+        }
     }
 
 }
