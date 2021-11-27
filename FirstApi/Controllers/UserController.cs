e1db5bd643287331842cf910d4e603a70aee7392
dodididop <digdemcokyasar@gmail.com>
2021-11-27 21:43:54 +0300

    Creating User class and initial user data. Getting list of the user.

--- FirstApi/Controllers/UserController.cs
+++ FirstApi/Controllers/UserController.cs
@@ -1,8 +1,31 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
-namespace FirstApi.Controllers{
-    public class FirstApi : ControllerBase{
-        
+namespace FirstApi.AddControllers{
+
+    [ApiController]
+    [Route("[controller]s")]
+    public class UserController : ControllerBase{
+        private static List<User> UserList = new List<User>(){
+            new User{
+                Id = 1,
+                Name = "Dido",
+                SurName = "Hello",
+                Password = "asdasd"
+            },
+            new User{
+                Id = 2,
+                Name = "Dilo",
+                SurName = "Hi",
+                Password = "123456"
+            }
+        };
+        [HttpGet]
+        public List<User> GetUsers(){
+            var userList = UserList.OrderBy(x=>x.Id).ToList<User>();
+            return userList;
+        }
     }
 
 }
